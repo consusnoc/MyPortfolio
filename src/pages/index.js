88a38889b837a4b8d9739a54d115d9cf0f9e6824
consusnoc <consueloleusnoc@gmail.com>
2019-08-12 19:26:28 -0300

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default () => <div>
+                        <h1>Coming soon...</h1>
+                        <p>Meanwhile you can find me + my work here</p>
+                        <a href="https://www.linkedin.com/in/consuelo-romano/" target="_blank" rel="noopener noreferrer">Linkedin</a>
+                        <a href="https://dribbble.com/consueloromano" target="_blank" rel="noopener noreferrer">Dribbble</a>
+                        <a href="https://www.behance.net/consueloromano" target="_blank" rel="noopener noreferrer">Behance</a>
+                     </div>
