b38424c5160fb1773d8208bd8065b8d9dd63506b
consusnoc <consueloleusnoc@gmail.com>
2019-10-21 00:30:14 -0300

    edit project pages and contentful blog posts

--- src/templates/project.js
+++ src/templates/project.js
@@ -26,13 +26,17 @@ const Project = ( props ) =>  {
     const options = {
         renderNode : {
             "embedded-asset-block": (node) => {
+                if(node.data.target.fields.title['en-US'] === "video carpool light") {
+                    const videoUrl = node.data.target.fields.file['en-US'].url
+                    return <video width="900" height="auto" controls><source src={videoUrl} type="video/mp4" /></video>
+                }
+                console.log(node)
                 const alt = node.data.target.fields.title['en-US']
                 const url = node.data.target.fields.file['en-US'].url
                 return <img alt={alt} src={url} />
             }
         }
     }
-
     return(
         <Layout>
             <Link to='/projects' className={projStyles.backLink}><img src={arrow} alt="arrow"/> Volver</Link>
